d4e8f270f18d63a94d51c723d7d812fcb949c524
maksimok93 <maksimok.dnepr@gmail.com>
2020-09-30 11:21:16 +0300

    Initialized cart_spec.rb

---
+++ spec/features/cart_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../spec_helper'
+
+feature 'RSpec: practical task_2, Rozetka UI Testing', type: :feature do
+  before(:each) do
+    visit('/')
+  end
+
+  describe 'When on Search Page' do
+    it 'Verifies adding product to the cart' do
+
+    end
+
+  describe 'When on Product Page' do
+    it 'Verifies adding product to the cart' do
+
+    end
+
+end

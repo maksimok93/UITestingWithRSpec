1070da1b15bcb6b33da6d0f51c3df11348298d9b
maksimok93 <maksimok.dnepr@gmail.com>
2020-10-07 15:50:48 +0300

    Feat
    Add to CartPage class next methods: refresh_cart; choose_additional_options; remove_from_cart

--- pages/cart_page.rb
+++ pages/cart_page.rb
@@ -9,4 +9,20 @@ class CartPage
   def click_go_to_checkout
     find(:xpath, CartLocators::CHECKOUT).click
   end
+
+  def refresh_cart
+    visit current_path
+    find(CartLocators::OPENCART).click
+  end
+
+  def choose_additional_options(product_name, option)
+    find(:xpath, "#{CartLocators::TITLE}, '#{product_name}')]#{CartLocators::OPTIONS}").click
+    find(:xpath, "//span[contains(text(), '#{option}')]").set(true)
+  end
+
+  def remove_from_cart(product_name)
+    find(:xpath, "#{CartLocators::TITLE}, '#{product_name}')]#{CartLocators::REMOVE_CONTAINER}").click
+    # find(CartLocators::ACTIONS).click
+    find(:xpath, "#{CartLocators::TITLE}, '#{product_name}')]#{CartLocators::REMOVE_PRODUCT}").click
+  end
 end

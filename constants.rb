cadc7a2286986b100078ab11879b8c4ced4c3400
maksimok93 <maksimok.dnepr@gmail.com>
2020-11-02 14:15:17 +0200

    Fix
    Change WATCH constant

--- constants.rb
+++ constants.rb
@@ -2,7 +2,7 @@ module ProductsConstants
   XIAOMI = 'Силиконовый чехол BoxFace Xiaomi Mi Play'
   REDMI = 'Мобильный телефон Xiaomi Redmi Note 9 Pro 6/64GB Tropical Green'
   DELL = 'G3500F716S1TN2060L-10BK'
-  WATCH = 'Смарт-часы Amazfit GTR 47mm Stainless Steel (514738)'
+  WATCH = 'Смарт-часы Amazfit GTR 47mm Titanium (514739)'
   SAMSUNG = 'Телевизор Samsung UE50NU7002UXUA'
 end
 

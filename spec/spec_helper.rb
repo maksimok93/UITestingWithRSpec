5c6c8f8a9489fb58711c551aca00e0b3e8e93149
maksimok93 <maksimok.dnepr@gmail.com>
2020-11-02 13:12:18 +0200

    Fix
    Add chromedriver to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,24 @@ $options = AdditionalOptions.new
 $product = CartPage.new
 
 Capybara.register_driver :selenium do |app|
+  Selenium::WebDriver::Chrome::Service.driver_path = '/usr/local/bin/chromedriver'
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
 Capybara.run_server = false
 Capybara.default_driver = :selenium
 Capybara.app_host = 'https://rozetka.com.ua'
+
+
+# /usr/local/bin/chromedriver
+
+# Instead of sleeping waits for ajax requests to end
+# def wait_for_ajax
+#   Timeout.timeout(Capybara.default_max_wait_time) do
+#     loop until finished_all_ajax_requests?
+#   end
+# end
+#
+# def finished_all_ajax_requests?
+#   page.evaluate_script('jQuery.active').zero?
+# end

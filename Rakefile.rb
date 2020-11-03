26a90807f3252fc6c09458ad95ad903b6eb5c7b9
maksimok93 <maksimok.dnepr@gmail.com>
2020-11-03 14:45:04 +0200

    Fix
    Update system command to run rake rspec task

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,7 +1,7 @@
 
 desc 'Running tests'
 task :rspec do
-  sh 'bundle exec rake -f /Users/mbezr/RubymineProjects/RSpec_Home_Task/Rakefile.rb my_task:do_stuff'
+  sh 'bundle exec rspec spec'
 end
 
 desc 'Rubocop linting task'

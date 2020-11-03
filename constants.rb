81559d93c47f26b8ff413b0f755e2fc676c9b9e3
maksimok93 <maksimok.dnepr@gmail.com>
2020-11-03 15:01:10 +0200

    Fix
    Update ProductsConstants

--- constants.rb
+++ constants.rb
@@ -1,9 +1,9 @@
 module ProductsConstants
   XIAOMI = 'Силиконовый чехол BoxFace Xiaomi Mi Play'
-  REDMI = 'Мобильный телефон Xiaomi Redmi Note 9 Pro 6/64GB Tropical Green'
+  REDMI = 'Мобильный телефон Xiaomi Redmi Note 9 Pro 6/64GB'
   DELL = 'G3500F716S1TN2060L-10BK'
   WATCH = 'Смарт-часы Amazfit GTR 47mm Titanium (514739)'
-  SAMSUNG = 'Телевизор Samsung UE50NU7002UXUA'
+  SAMSUNG = 'Телевизор Samsung UE50TU7002UXUA'
 end
 
 module Components

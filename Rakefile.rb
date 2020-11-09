8e23b9bc0819a006e20f28be4277be5fa3117bfa
maksimok93 <maksimok.dnepr@gmail.com>
2020-11-09 09:42:15 +0200

    Docs
    Add ci report to rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,9 +1,15 @@
 require 'rubocop/rake_task'
+require 'ci/reporter/rake/rspec'
 
 desc 'Running tests'
-task :rspec do
+
+task :rspec => 'ci:setup:rspec' do
   sh 'bundle exec rspec spec'
 end
+
+# task :rspec do
+#   sh 'bundle exec rspec spec'
+# end
 
 desc 'Rubocop linting task'
 task :rubocop do

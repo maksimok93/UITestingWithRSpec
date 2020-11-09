9ba93ffae4079b1772df97a057103ee1cfe72016
maksimok93 <maksimok.dnepr@gmail.com>
2020-11-09 18:27:25 +0200

    Fix
    Update default task in Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,13 +1,17 @@
 require 'rubocop/rake_task'
 require 'ci/reporter/rake/rspec'
+require 'rspec/core/rake_task'
+
+RSpec::Core::RakeTask.new(:spec) do |test|
+  test.pattern = "spec/features/*_spec.rb"
+end
+
+task :default => :spec
 
 desc 'Running tests'
-
-
-task :spec => 'ci:setup:rspec'
-# task :rspec do
-#   sh 'bundle exec rspec spec'
-# end
+task :rspec do
+  sh 'bundle exec rspec spec'
+end
 
 desc 'Rubocop linting task'
 task :rubocop do

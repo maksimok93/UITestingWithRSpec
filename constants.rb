c3dea0b2756791d5a32d444f01ca801ae71364a4
maksimok93 <maksimok.dnepr@gmail.com>
2020-11-19 16:37:58 +0200

    Docs
    Add ProductAttributes class

--- constants.rb
+++ constants.rb
@@ -1,7 +1,11 @@
 module ProductsConstants
-  XIAOMI = 'Силиконовый чехол BoxFace Xiaomi Mi Play'
-  REDMI = 'Мобильный телефон Xiaomi Redmi Note 9 Pro 6/64GB'
-  DELL = 'G3500F716S1TN2060L-10BK'
+  XIAOMI = 'Силиконовый чехол BoxFace Xiaomi Mi Play Динамо Киев'
+  REDMI = 'Наушники Xiaomi Redmi Airdots Basic S TWSEJ05LS TWS Black'
+  DELL = 'Dell G3 3500 (G3500F716S1TN2060L-10BK)'
   WATCH = 'Смарт-часы Amazfit T-Rex Rock Black (601683)'
   SAMSUNG = 'Телевизор Samsung UE50TU7002UXUA'
+end
+
+module ProductAttributes
+  PRICE = "div[@class='goods-tile__prices']//div[@class='goods-tile__price']//p//span[@class='goods-tile__price-value']"
 end
